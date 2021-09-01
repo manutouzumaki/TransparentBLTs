3665603474ef44161e508d0f2df243188ba0746b
manuelcabrerizo <manuelcabrerizo5@gmail.com>
2021-09-01 16:00:02 -0300

    add support for 8-bits images

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,11 +9,19 @@ struct bit_map_header
 	WORD   Reserved1;       /* Always 0 */
 	WORD   Reserved2;       /* Always 0 */
 	DWORD  BitmapOffset;    /* Starting position of image data in bytes */
+
 	DWORD  Size;            /* Size of this header in bytes */
 	LONG   Width;           /* Image width in pixels */
 	LONG   Height;          /* Image height in pixels */
 	WORD   Planes;          /* Number of color planes */
 	WORD   BitsPerPixel;    /* Number of bits per pixel */
+    DWORD Compression;     /* Compression methods used */
+	DWORD SizeOfBitmap;    /* Size of bitmap in bytes */
+	LONG  HorzResolution;  /* Horizontal resolution in pixels per meter */
+	LONG  VertResolution;  /* Vertical resolution in pixels per meter */
+	DWORD ColorsUsed;      /* Number of colors in the image */
+	DWORD ColorsImportant; /* Minimum number of important colors */
+	
 };
 #pragma pack(pop)
 
@@ -22,6 +30,8 @@ struct bit_map
     void *Pixels;
     int Width;
     int Height;
+    int BitsPerPixel;
+    unsigned int *ColorPalette;
 };
 
 static void *
@@ -55,16 +65,124 @@ LoadBMP(char *FileName)
         OutputDebugString("ERROR::LOADING::BMP\n");
         return Result;
     }
+
     bit_map_header *BMPHeader = (bit_map_header *)BMPFile;
+
     Result.Width = BMPHeader->Width;
     Result.Height = BMPHeader->Height;
+    Result.BitsPerPixel = BMPHeader->BitsPerPixel;
+
+    if(Result.BitsPerPixel == 8)
+    {
+        Result.ColorPalette = (unsigned int *)((unsigned char *)BMPFile + sizeof(bit_map_header));
+        int StopHere = 0;
+    }
     Result.Pixels = (void *)((unsigned char *)BMPFile + BMPHeader->BitmapOffset);
-    int StopHere = 0;
+
     return Result;
 }
 
 static void 
-TransparentBlt(void *DestBuffer, BITMAPINFOHEADER *DestHeader,
+Bits8TransparentBlt(void *DestBuffer, BITMAPINFOHEADER *DestHeader,
+               int XDest, int YDest, bit_map SourceBitmap)
+{
+    unsigned int *DestPtr = (unsigned int *)DestBuffer;
+    unsigned char *SourcePtr = (unsigned char *)SourceBitmap.Pixels;
+
+    int DestDeltaScan, DestWidthBytes, DestRealHeight;
+    int SourceDeltaScan, XSource = 0, YSource = 0;
+    int Width, Height;
+
+    DestWidthBytes = DestHeader->biWidth; 
+    // check if we have bottom-up or top-down BackBuffer setup
+    if(DestHeader->biHeight < 0)
+    {
+        // Dest if Top-Down
+        DestRealHeight = -DestHeader->biHeight; // get pisitive height
+        DestDeltaScan = DestWidthBytes; // travel down dest
+    }
+    else
+    {
+        // Dest if Bottom-Up
+        DestRealHeight = DestHeader->biHeight; 
+        DestDeltaScan = -DestWidthBytes; // travel down dest
+        // point to top scanline
+        DestPtr += DestHeader->biWidth*DestHeader->biHeight - DestWidthBytes;
+    }
+
+    // Clip the source to dest
+    Width = SourceBitmap.Width;
+    Height = SourceBitmap.Height;
+    if(XDest < 0)
+    {
+        // left clipped
+        Width += XDest;
+        XSource = -XDest;
+        XDest = 0;
+    }
+    if((XDest + Width) > DestHeader->biWidth)
+    {
+        // right clipped
+        Width = DestHeader->biWidth - XDest;
+    }
+    if(YDest < 0)
+    {
+        // top clipped
+        Height += YDest;
+        YSource = -YDest;
+        YDest = 0;
+    }
+    if((YDest + Height) > DestRealHeight)
+    {
+        // bottom clipped
+        Height = DestRealHeight - YDest;
+    }
+    
+    SourceDeltaScan = SourceBitmap.Width;
+
+
+    // step to starting source pixel
+    if(XSource != 0)
+    {
+        SourcePtr += (SourceBitmap.Width*SourceBitmap.Height - Width) - (YSource*SourceDeltaScan);
+    }
+    else
+    {
+        SourcePtr += (SourceBitmap.Width*SourceBitmap.Height - SourceDeltaScan) - (YSource*SourceDeltaScan);
+    }
+
+    DestPtr += (YDest*DestDeltaScan)+XDest;
+
+
+    // account for processed span in delta scans
+    SourceDeltaScan += Width;
+    DestDeltaScan -= Width;
+
+    if((Height > 0) && (Width > 0))
+    {
+        // we have something to BLT
+        for(int Y = 0;
+            Y < Height;
+            ++Y)
+        {
+            for(int X = 0;
+                X < Width;
+                ++X)
+            {
+                unsigned int Bits32SourceValue = SourceBitmap.ColorPalette[*SourcePtr];
+                *DestPtr = Bits32SourceValue; // copy the pixel
+                DestPtr++;
+                SourcePtr++;
+            }
+            DestPtr += DestDeltaScan;
+            SourcePtr -= SourceDeltaScan;
+        }
+    }
+
+}
+
+static void 
+Bits32TransparentBlt(void *DestBuffer, BITMAPINFOHEADER *DestHeader,
                int XDest, int YDest, bit_map SourceBitmap)
 {
     unsigned int *DestPtr = (unsigned int *)DestBuffer;
@@ -205,7 +323,7 @@ int WINAPI WinMain(HINSTANCE Instance,
         BITMAPINFO BackBufferInfo = {};
         BackBufferInfo.bmiHeader.biSize = sizeof(BackBufferInfo.bmiHeader);
         BackBufferInfo.bmiHeader.biWidth = Width;
-        BackBufferInfo.bmiHeader.biHeight = Height;
+        BackBufferInfo.bmiHeader.biHeight = -Height;
         BackBufferInfo.bmiHeader.biPlanes = 1;
         BackBufferInfo.bmiHeader.biBitCount = 32;
         BackBufferInfo.bmiHeader.biCompression = BI_RGB;
@@ -213,17 +331,11 @@ int WINAPI WinMain(HINSTANCE Instance,
         HDC DeviceContext = GetDC(Window);
         HBITMAP BackBufferHandle = CreateDIBSection(DeviceContext, &BackBufferInfo,
                                                     DIB_RGB_COLORS, &BackBuffer, NULL, NULL);        
-        bit_map SabrinaBMP = LoadBMP("../Data/Sabrina.bmp");
-        if(SabrinaBMP.Pixels)
-        {
-            OutputDebugString("BMP Loaded\n");
-        }
 
-        bit_map CamilaBMP = LoadBMP("../Data/Camila.bmp");
-        if(CamilaBMP.Pixels)
-        { 
-            OutputDebugString("BMP Loaded\n");
-        }
+        bit_map Sabrina32BMP = LoadBMP("../Data/Sabrina.bmp");
+        bit_map Sabrina8BMP = LoadBMP("../Data/Sabrina1.bmp");
+        bit_map Camila32BMP = LoadBMP("../Data/Camila.bmp");
+        bit_map Camila8BMP = LoadBMP("../Data/Camila1.BMP");
 
         unsigned int *Buffer = (unsigned int *)BackBuffer;
         for(int Y = 0;
@@ -238,10 +350,15 @@ int WINAPI WinMain(HINSTANCE Instance,
             }
         }
 
-        TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
-                       0, 0, SabrinaBMP);
-        TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
-                       225, 0, CamilaBMP);
+        
+        Bits32TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
+                       0, 0, Sabrina32BMP);
+        Bits8TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
+                       256, 0, Sabrina8BMP);
+        Bits32TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
+                             0, 256, Camila32BMP);
+        Bits8TransparentBlt(BackBuffer, &BackBufferInfo.bmiHeader,
+                       128, 256, Camila8BMP);
 
 
         GlobalRunning = true;
